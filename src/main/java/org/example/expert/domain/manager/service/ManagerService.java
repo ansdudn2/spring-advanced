62024a0657aee6e46071cde91f123024bc7f987c
ansdudn2 <ansdudn2@naver.com>
2025-02-27 11:40:27 +0900

    Lv3-2 casethree retouch and finish

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -36,10 +36,6 @@ public class ManagerService {
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         if (todo.getUser() == null) {
-            throw new InvalidRequestException("Todo의 user가 존재하지 않습니다.");
-        }
-
-        if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
 
